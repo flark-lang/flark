5b98f04d1177947e866abd4ec9ad839dc77eb34e
Gnlow <gnlow@protonmail.com>
2023-07-29 16:22:32 +0900

    add interpolation, style

--- src/highlight.js
+++ src/highlight.js
@@ -1,6 +1,11 @@
 import { styleTags, tags as t } from "@lezer/highlight"
 
 export const flarkHighlighting = styleTags({
-    String: t.content,
+    PlainText: t.content,
     "VPipe HPipe": t.operator,
+    "LBrace RBrace": t.brace,
+    "LBracket RBracket": t.squareBracket,
+    Number: t.number,
+    Identifier: t.name,
+    Dot: t.operator,
 })
